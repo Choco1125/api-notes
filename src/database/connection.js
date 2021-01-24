14b702b74bb8882285fc9a2cc8e3b7cdefaf6e2c
Choco1125 <sebascruz981125@gmail.com>
2021-01-23 22:48:22 -0500

    Changing database connection to async/await

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'api_notes'
-});
+async function connect(){
+	try{
+		const connection = await mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'api_notes'
+		});
+		return connection;
+	
+	}catch(e){
+		console.log(e);
+	}
+}
 
-module.exports = connection;
+	module.exports = connect;
