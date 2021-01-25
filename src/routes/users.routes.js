dc31ba0d51b795534dbb707d20a47522715d1b5b
Choco1125 <sebascruz981125@gmail.com>
2021-01-24 21:11:15 -0500

    (User Routes): Adding route [DELETE] to delete user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const userController = require('./../controllers/user.controller');
 router.get('/', userController.getAll);
 router.post('/', userController.create);
 router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 module.exports = router;
