2ce4028b68f459af85728f5a52788200f6d4a147
Choco1125 <sebascruz981125@gmail.com>
2021-01-24 20:55:55 -0500

    (User Routes): Adding route [PUT] to update user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,5 +6,6 @@ const userController = require('./../controllers/user.controller');
 
 router.get('/', userController.getAll);
 router.post('/', userController.create);
+router.put('/:id', userController.update);
 
 module.exports = router;
