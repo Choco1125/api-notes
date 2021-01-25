dea8d85260d6a0df2190f3d24fc985514352751a
Choco1125 <sebascruz981125@gmail.com>
2021-01-24 20:54:14 -0500

    (User Controller): Adding update data user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { updateEmail } = require("./../models/user.model");
 const userModel = require("./../models/user.model");
 
 const userController = {
@@ -12,6 +13,19 @@ const userController = {
 		let saved = await userModel.create(email, password);
 		if (!saved) return res.status(500).json({ message: "User already exists." });
 		res.status(201).json({ message: "User created." });
+	},
+	async update(req, res) {
+		const { email, password } = req.body;
+		const { id } = req.params;
+		let saved = false;
+		if (email) {
+			saved = await userModel.updateEmail(email, id);
+		}
+		if (password) {
+			saved = await userModel.updatePassword(password, id);
+		}
+		if (!saved) return res.status(500).json({ message: "Can not update user data." });
+		res.status(200).json({ message: "User updated." });
 	}
 }
 
