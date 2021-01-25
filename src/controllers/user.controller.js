61cda7c38f8afc3164e1cd88c821f1d00fc15505
Choco1125 <sebascruz981125@gmail.com>
2021-01-24 19:02:29 -0500

    (User Controller): Adding create user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,17 @@ const userModel = require("./../models/user.model");
 
 const userController = {
 
-  async getAll(req, res) {
+	async getAll(req, res) {
 		const data = await userModel.getAll();
 		res.status(200).json(data);
-  }
+	},
+	async create(req, res) {
+		const { email, password } = req.body;
+		if (!email || !password) return res.status(400).json({ message: "You must provide required data." });
+		let saved = await userModel.create(email, password);
+		if (!saved) return res.status(500).json({ message: "Saved user failed." });
+		res.status(201).json({ message: "User created." });
+	}
 
 }
 
