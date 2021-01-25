08991194d48110a9cf8574f6e54f05d2e5310e20
Choco1125 <sebascruz981125@gmail.com>
2021-01-24 19:03:49 -0500

    (User Model): Inserting new user to DB

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,24 @@
 const connect = require('./../database/connection');
 
 const userModel = {
-	async getAll(){
-		try{
+	async getAll() {
+		try {
 			const database = await connect();
 			const [rows, fields] = await database.query("SELECT * FROM users");
-			return rows;	
-		}catch(e){
-			console.log(`exception: ${e}`);
+			return rows;
+		} catch (e) {
+			console.log(`Get all user exception: ${e}`);
+		}
+	},
+	async create(email, password) {
+		try {
+			const database = await connect();
+			const query = "INSERT INTO users (email,password) VALUES (?,?)";
+			await database.execute(query, [email, password]);
+			return true;
+		} catch (e) {
+			console.log(`Create user exception: ${e}`);
+			return false;
 		}
 	}
 }
